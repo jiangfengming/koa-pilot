ec876ed494deeff3219729f0f6c9cd00291fc5c8
Jiang Fengming <jiangfengming87@gmail.com>
2020-12-07 13:17:40 +0800

    update deps; update eslint rule;

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
-const Koa = require('koa')
-const Router = require('.')
+const Koa = require('koa');
+const Router = require('.');
 
-const app = new Koa()
+const app = new Koa();
 
 const router = new Router([
   ['GET', '/', ctx => ctx.body = 'hello world'],
   ['/user/:username', ctx => ctx.body = `hello ${ctx.params.string('username')}. code: ${ctx.queries.int('code')}`]
-])
+]);
 
-router.get('(.*)', ctx => ctx.body = '404')
+router.get('(.*)', ctx => ctx.body = '404');
 
-app.use(router.middleware)
-app.listen(3000)
+app.use(router.middleware);
+app.listen(3000);
